1e5c9e004c65be3397d4655d2d9333efef0ccc19
nhrafi1x-ui <nhrafi1x@gmail.com>
2026-04-05 00:39:59 +0600

    Change Firestore Database ID in firebase.ts
    Updated Firestore Database ID in Firebase configuration.

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,5 +1,4 @@
-
-  import { initializeApp } from "firebase/app";
+import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
@@ -10,18 +9,11 @@ const firebaseConfig = {
   projectId: "gen-lang-client-0454648898",
   storageBucket: "gen-lang-client-0454648898.firebasestorage.app",
   messagingSenderId: "464581174826",
-  
-  
   appId: "1:464581174826:web:e36c502d95eb7e61eac553",
-  // If you want to use Realtime Database, add this line:
-  databaseURL: "https://gen-lang-client-0454648898-default-rtdb.firebaseio.com/",
-  // Firestore ID is almost always "(default)"
-  firestoreDatabaseId: "(default)" 
+  firestoreDatabaseId: "ai-studio-af7e3e39-7648-41aa-816c-742007de4ae3"
 };
 
 // Initialize Firebase SDK
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-
-// Initialize Firestore with the correct Database ID string
 export const db = getFirestore(app, firebaseConfig.firestoreDatabaseId);
