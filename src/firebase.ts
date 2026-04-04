a8a526b088762c4786ca4d36135026d8a85ede74
nhrafi1x-ui <nhrafi1x@gmail.com>
2026-04-04 23:17:24 +0600

    Update firestoreDatabaseId to use database URL

--- src/firebase.ts
+++ src/firebase.ts
@@ -10,7 +10,7 @@ const firebaseConfig = {
   storageBucket: "gen-lang-client-0454648898.firebasestorage.app",
   messagingSenderId: "464581174826",
   appId: "1:464581174826:web:e36c502d95eb7e61eac553",
-  firestoreDatabaseId: "firebase-applet-config.json"
+  firestoreDatabaseId: "https://gen-lang-client-0454648898-default-rtdb.firebaseio.com/"
 };
 
 // Initialize Firebase SDK
