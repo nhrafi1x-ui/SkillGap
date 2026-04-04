33b31acf4f0f00c314a15e7b3674b5135014daba
TASHPIA AFROZ NIJHUM <nijhum23105341022@diu.edu.bd>
2026-04-05 00:38:08 +0600

    Fix import statement formatting in firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,4 +1,5 @@
-import { initializeApp } from "firebase/app";
+
+  import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
@@ -9,6 +10,8 @@ const firebaseConfig = {
   projectId: "gen-lang-client-0454648898",
   storageBucket: "gen-lang-client-0454648898.firebasestorage.app",
   messagingSenderId: "464581174826",
+  
+  
   appId: "1:464581174826:web:e36c502d95eb7e61eac553",
   // If you want to use Realtime Database, add this line:
   databaseURL: "https://gen-lang-client-0454648898-default-rtdb.firebaseio.com/",
